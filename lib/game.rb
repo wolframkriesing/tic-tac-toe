a7bea596b42eb274c4c4b44cd4d013e8b3aa409e
Wolfram Kriesing <wk@uxebu.com>
2015-09-01 22:51:42 +0200

    Splitting up unreadable long method.

--- lib/game.rb
+++ lib/game.rb
@@ -135,12 +135,22 @@ def tie(board)
 end
 
 def is_game_over(board)
+  any_complete_row(board) || any_complete_column(board) || any_diagonal_complete(board)
+end
+
+def any_complete_row(board)
   [board[0], board[1], board[2]].uniq.length == 1 ||
   [board[3], board[4], board[5]].uniq.length == 1 ||
-  [board[6], board[7], board[8]].uniq.length == 1 ||
+  [board[6], board[7], board[8]].uniq.length == 1
+end
+
+def any_complete_column(board)
   [board[0], board[3], board[6]].uniq.length == 1 ||
   [board[1], board[4], board[7]].uniq.length == 1 ||
-  [board[2], board[5], board[8]].uniq.length == 1 ||
+  [board[2], board[5], board[8]].uniq.length == 1
+end
+
+def any_diagonal_complete(board)
   [board[0], board[4], board[8]].uniq.length == 1 ||
   [board[2], board[4], board[6]].uniq.length == 1
 end
