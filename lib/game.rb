9ecfe760090892b5e7ea2d9f19d44d88b43d75e6
Wolfram Kriesing <wk@uxebu.com>
2015-09-01 22:39:25 +0200

    Move static function out of class scope.

--- lib/game.rb
+++ lib/game.rb
@@ -117,20 +117,20 @@ class Game
     n = rand(0..available_cells_indexes.count)
     available_cells_indexes[n].to_i
   end
+  
+end
 
-  def is_game_over(board)
-    [board[0], board[1], board[2]].uniq.length == 1 ||
-    [board[3], board[4], board[5]].uniq.length == 1 ||
-    [board[6], board[7], board[8]].uniq.length == 1 ||
-    [board[0], board[3], board[6]].uniq.length == 1 ||
-    [board[1], board[4], board[7]].uniq.length == 1 ||
-    [board[2], board[5], board[8]].uniq.length == 1 ||
-    [board[0], board[4], board[8]].uniq.length == 1 ||
-    [board[2], board[4], board[6]].uniq.length == 1
-  end
+def tie(board)
+  board.all? { |cell| cell == "X" || cell == "O" }
+end
 
-  def tie(board)
-    board.all? { |cell| cell == "X" || cell == "O" }
-  end
-
+def is_game_over(board)
+  [board[0], board[1], board[2]].uniq.length == 1 ||
+  [board[3], board[4], board[5]].uniq.length == 1 ||
+  [board[6], board[7], board[8]].uniq.length == 1 ||
+  [board[0], board[3], board[6]].uniq.length == 1 ||
+  [board[1], board[4], board[7]].uniq.length == 1 ||
+  [board[2], board[5], board[8]].uniq.length == 1 ||
+  [board[0], board[4], board[8]].uniq.length == 1 ||
+  [board[2], board[4], board[6]].uniq.length == 1
 end
