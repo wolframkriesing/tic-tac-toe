eb04e2ac86e027f7d9a0f7e0b495d3c384b4b63e
Wolfram Kriesing <wk@uxebu.com>
2015-09-01 19:42:45 +0200

    Make board printing a bit more readable.

--- lib/game.rb
+++ lib/game.rb
@@ -14,7 +14,9 @@ class Game
   end
 
   def print_board(board)
-    puts "|_#{board[0]}_|_#{board[1]}_|_#{board[2]}_|\n|_#{board[3]}_|_#{board[4]}_|_#{board[5]}_|\n|_#{board[6]}_|_#{board[7]}_|_#{board[8]}_|\n"
+    puts "|_#{board[0]}_|_#{board[1]}_|_#{board[2]}_|\n" <<
+         "|_#{board[3]}_|_#{board[4]}_|_#{board[5]}_|\n" <<
+         "|_#{board[6]}_|_#{board[7]}_|_#{board[8]}_|\n"
   end
 
   def game_play_loop(board)
