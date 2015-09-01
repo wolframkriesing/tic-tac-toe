b0b38c0b7f5363903109e25f93a78703ba9a9fc8
Wolfram Kriesing <wk@uxebu.com>
2015-09-01 23:16:35 +0200

    Use self where possible.

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,13 @@
 class Board < Array 
   
   def print()
-    board = self
     puts
     print_board_border
-    print_board_line(board[0..2])
+    print_board_line(self[0..2])
     print_board_border
-    print_board_line(board[3..5])
+    print_board_line(self[3..5])
     print_board_border
-    print_board_line(board[6..8])
+    print_board_line(self[6..8])
     print_board_border
     puts
   end
