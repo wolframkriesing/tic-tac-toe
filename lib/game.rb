767cb20384412d13e6e09f3e5de342b50c8ccac0
Wolfram Kriesing <wk@uxebu.com>
2015-09-01 23:25:16 +0200

    Move game over check into GameRules.

--- lib/game.rb
+++ lib/game.rb
@@ -20,25 +20,6 @@ class Board < Array
     puts "+---+---+---+"
   end
 
-  def has_completed_row
-    board = self
-    [board[0], board[1], board[2]].uniq.length == 1 ||
-    [board[3], board[4], board[5]].uniq.length == 1 ||
-    [board[6], board[7], board[8]].uniq.length == 1
-  end
-
-  def has_completed_column
-    board = self
-    [board[0], board[3], board[6]].uniq.length == 1 ||
-    [board[1], board[4], board[7]].uniq.length == 1 ||
-    [board[2], board[5], board[8]].uniq.length == 1
-  end
-  
-  def has_completed_diagonal
-    board = self
-    [board[0], board[4], board[8]].uniq.length == 1 ||
-    [board[2], board[4], board[6]].uniq.length == 1
-  end
 end
 
 class Game
@@ -153,5 +134,37 @@ def tie(board)
 end
 
 def is_game_over(board)
-  board.has_completed_row || board.has_completed_column || board.has_completed_diagonal
+  GameRules.new(board).is_game_over  
+end
+
+class GameRules
+  
+  def initialize(board)
+    @board = board
+  end
+  
+  def is_game_over
+    has_completed_row || has_completed_column || has_completed_diagonal
+  end
+  
+  def has_completed_row
+    board = @board
+    [board[0], board[1], board[2]].uniq.length == 1 ||
+    [board[3], board[4], board[5]].uniq.length == 1 ||
+    [board[6], board[7], board[8]].uniq.length == 1
+  end
+
+  def has_completed_column
+    board = @board
+    [board[0], board[3], board[6]].uniq.length == 1 ||
+    [board[1], board[4], board[7]].uniq.length == 1 ||
+    [board[2], board[5], board[8]].uniq.length == 1
+  end
+  
+  def has_completed_diagonal
+    board = @board
+    [board[0], board[4], board[8]].uniq.length == 1 ||
+    [board[2], board[4], board[6]].uniq.length == 1
+  end
+
 end
