6ff220c8aaa19463001fd10cf87ffddbb29a6d3f
Wolfram Kriesing <wk@uxebu.com>
2015-09-08 01:38:59 +0200

    Output the board using the array's index.

--- lib/board_output.rb
+++ lib/board_output.rb
@@ -3,19 +3,24 @@ class BoardOutput
   def self.print(board)
     puts
     print_board_border
-    print_board_line(board[0..2])
+    print_board_line(board, 0, 1, 2)
     print_board_border
-    print_board_line(board[3..5])
+    print_board_line(board, 3, 4, 5)
     print_board_border
-    print_board_line(board[6..8])
+    print_board_line(board, 6, 7, 8)
     print_board_border
     puts
   end
   
 end
 
-def print_board_line(board)
-  puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+def print_board_line(board, cell1, cell2, cell3)
+  puts "| #{print_cell(board, cell1)} | #{print_cell(board, cell2)} | #{print_cell(board, cell3)} |"
+end
+
+def print_cell(board, index)
+  cell = board[index]
+  cell.is_available? ? index.to_s : cell.player.name
 end
 
 def print_board_border
