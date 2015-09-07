b36f5aeda78b8fa1338577cc479949fc2c300791
Wolfram Kriesing <wk@uxebu.com>
2015-09-07 23:17:24 +0200

    Use player instances.

--- specs/winning_move_spec.rb
+++ specs/winning_move_spec.rb
@@ -1,51 +1,41 @@
 require_relative "../lib/winning_move"
-require_relative "../lib/cell"
+# require_relative "../lib/cell"
 require_relative "./boards"
 require "minitest/autorun"
 
 class MyBoards < Boards
   
   def self.fill_cell_0_to_win(player)
-	[
-		Cell.new("0"), occupied_cell_by("1", player), occupied_cell_by("2", player),
-		Cell.new("3"), Cell.new("4"), Cell.new("5"), 
-		Cell.new("6"), Cell.new("7"), Cell.new("8"), 
-	]
+    [
+      Cell.new("0"), occupied_cell_by("1", player), occupied_cell_by("2", player),
+      Cell.new("3"), Cell.new("4"), Cell.new("5"), 
+      Cell.new("6"), Cell.new("7"), Cell.new("8"), 
+    ]
   end
   
   private 
   
   def self.occupied_cell_by(x,y)
-	Boards.occupied_cell_by(x,y)
+	  cell = Boards.occupied_cell_by(x,y)
+    cell
   end
 	 
 end
 
-class SafteyNetTest < MiniTest::Unit::TestCase
-	
-  def test_dont_modify_given_board
-    # for lack of ruby skills, i was trapped by this :)
-    board = Boards.empty_board
-	  cell = WinningMove.new(board).calculate_cell_for_win("X")
-    assert_equal board, Boards.empty_board
-  end
-  
-end 
-  
 class WinningMoveTest < MiniTest::Unit::TestCase
 	
   def test_for_an_empty_board_there_is_none
-	  cell = WinningMove.new(Boards.empty_board).calculate_cell_for_win("X")
+	  cell = WinningMove.new(Boards.empty_board).calculate_cell_for_win(Boards.player1)
 	  assert_equal cell, nil
   end
   
   def test_fill_cell_0_to_win_for_player_1
-	  cell = WinningMove.new(MyBoards.fill_cell_0_to_win("X")).calculate_cell_for_win("X")
+	  cell = WinningMove.new(MyBoards.fill_cell_0_to_win(Boards.player1)).calculate_cell_for_win(Boards.player1)
 	  assert_equal cell, 0
   end
   
   def test_fill_cell_0_to_win_for_player_2
-	  cell = WinningMove.new(MyBoards.fill_cell_0_to_win("O")).calculate_cell_for_win("O")
+	  cell = WinningMove.new(MyBoards.fill_cell_0_to_win(Boards.player2)).calculate_cell_for_win(Boards.player2)
 	  assert_equal cell, 0
   end
   
