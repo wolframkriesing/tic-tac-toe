084fe4c92bd6db191faf876e281b4f02a5b72bda
Wolfram Kriesing <wk@uxebu.com>
2015-09-07 10:12:13 +0200

    Various win+tie combinations tested.

--- specs/game_rules_spec.rb
+++ specs/game_rules_spec.rb
@@ -4,7 +4,7 @@ require "minitest/autorun"
 class Boards
   def self.empty_board
 	[
-		Cell.new("0"), Cell.new("1"), Cell.new("2"), 
+		Cell.new("0"), Cell.new("1"), Cell.new("2"),
 		Cell.new("3"), Cell.new("4"), Cell.new("5"), 
 		Cell.new("6"), Cell.new("7"), Cell.new("8"), 
 	]
@@ -15,6 +15,30 @@ class Boards
 		occupied_cell_by("0", "X"), occupied_cell_by("1", "O"), occupied_cell_by("2", "O"), 
 		occupied_cell_by("3", "O"), occupied_cell_by("4", "X"), occupied_cell_by("5", "X"), 
 		occupied_cell_by("6", "O"), occupied_cell_by("7", "X"), occupied_cell_by("8", "O"), 
+	]
+  end
+  
+  def self.top_row_won_by(player)
+	[
+		occupied_cell_by("0", player), occupied_cell_by("1", player), occupied_cell_by("2", player),
+		Cell.new("3"), Cell.new("4"), Cell.new("5"), 
+		Cell.new("6"), Cell.new("7"), Cell.new("8"), 
+	]
+  end
+  
+  def self.diagonal_from_left_top_won_by(player)
+	[
+		occupied_cell_by("0", player), Cell.new("1"), Cell.new("2"),
+		Cell.new("3"), occupied_cell_by("4", player), Cell.new("5"), 
+		Cell.new("6"), Cell.new("7"), occupied_cell_by("8", player), 
+	]
+  end
+  
+  def self.diagonal_from_right_top_won_by(player)
+	[
+		Cell.new("0"), Cell.new("1"), occupied_cell_by("2", player),
+		Cell.new("3"), occupied_cell_by("4", player), Cell.new("5"), 
+		occupied_cell_by("6", player), Cell.new("7"), Cell.new("8") 
 	]
   end
   
@@ -31,6 +55,30 @@ class GameNotWon < MiniTest::Unit::TestCase
   def test_no_cell_occupied_game_is_not_won
 	game_won = GameRules.new(Boards.empty_board).is_game_won
 	assert_equal game_won, false  
+  end
+	
+  def test_tie_game_is_not_won
+	game_won = GameRules.new(Boards.tie_board).is_game_won
+	assert_equal game_won, false  
+  end
+
+end
+
+class GameWon < MiniTest::Unit::TestCase
+	
+  def test_top_row_occupied_by_player_is_won
+	game_won = GameRules.new(Boards.top_row_won_by("X")).is_game_won
+	assert_equal game_won, true  
+  end
+
+  def test_diagonal_from_left_top_occupied_by_player_is_won
+	game_won = GameRules.new(Boards.diagonal_from_left_top_won_by("X")).is_game_won
+	assert_equal game_won, true  
+  end
+
+  def test_diagonal_from_right_top_occupied_by_player_is_won
+	game_won = GameRules.new(Boards.diagonal_from_right_top_won_by("O")).is_game_won
+	assert_equal game_won, true  
   end
 
 end
