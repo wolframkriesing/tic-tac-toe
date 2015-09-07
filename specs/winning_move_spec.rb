b71d96db8fa09e6780390065d1927cd1b5a2e705
Wolfram Kriesing <wk@uxebu.com>
2015-09-07 12:14:03 +0200

    Get proper winning move.

--- specs/winning_move_spec.rb
+++ specs/winning_move_spec.rb
@@ -1,11 +1,39 @@
 require_relative "./boards"
 require "minitest/autorun"
 
+class MyBoards < Boards
+  
+  def self.fill_cell_0_to_win(player)
+	[
+		Cell.new("0"), occupied_cell_by("1", player), occupied_cell_by("2", player),
+		Cell.new("3"), Cell.new("4"), Cell.new("5"), 
+		Cell.new("6"), Cell.new("7"), Cell.new("8"), 
+	]
+  end
+  
+  private 
+  
+  def self.occupied_cell_by(x,y)
+	Boards.occupied_cell_by(x,y)
+  end
+	 
+end
+
 class WinningMoveTest < MiniTest::Unit::TestCase
 	
   def test_for_an_empty_board_there_is_none
 	  cell = WinningMove.new.calculate_cell(Boards.empty_board, "X", "O")
 	  assert_equal cell, nil
+  end
+  
+  def test_fill_cell_0_to_win_for_player_1
+	  cell = WinningMove.new.calculate_cell(MyBoards.fill_cell_0_to_win("X"), "X", "O")
+	  assert_equal cell, 0
+  end
+  
+  def test_fill_cell_0_to_win_for_player_2
+	  cell = WinningMove.new.calculate_cell(MyBoards.fill_cell_0_to_win("O"), "X", "O")
+	  assert_equal cell, 0
   end
   
 end
