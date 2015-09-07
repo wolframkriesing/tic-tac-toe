846dc75bb8f82f8edec17f4cad97885948ebdf3d
Wolfram Kriesing <wk@uxebu.com>
2015-09-07 22:58:34 +0200

    Make player store its name.

--- specs/boards.rb
+++ specs/boards.rb
@@ -24,6 +24,15 @@ class Cell
 end
 
 class Player
+  def initialize(name)
+    @name = name
+  end
+  def is?(other_player)
+    other_player == self
+  end
+  def to_s
+    return @name
+  end
 end
 
 player_1 = Player.new
