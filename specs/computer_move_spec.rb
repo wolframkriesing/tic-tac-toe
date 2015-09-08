ed177c7ac509504ceeb10638ab1223802575bb74
Wolfram Kriesing <wk@uxebu.com>
2015-09-08 13:13:55 +0200

    Shifting things around, so all board builder things can move to Boards.rb.

--- specs/computer_move_spec.rb
+++ specs/computer_move_spec.rb
@@ -5,26 +5,30 @@ require "minitest/autorun"
 class MyBoards
 	
   def self.full_board_but_one_cell(cell_index)
-	board = Boards.tie_board
-	board[cell_index] = Cell.new
-	board
+    board = Boards.tie_board
+    board[cell_index] = Cell.new
+    board
   end
   
   def self.to_board(s)
-	map = {
-	  "C" => Boards.player1,
-	  "H" => Boards.player2
-	}
-	board = s.split("").map {|char|
-	  cell = Cell.new 
-	  if map[char] != nil
-		cell.occupy_by(map[char]) 
-	  end
-	  cell
-	}
-	board
+    map = {
+      "C" => Boards.player1,
+      "H" => Boards.player2
+    }
+    board = s.split("").map {|char|
+      cell = Cell.new 
+      if map[char] != nil
+      cell.occupy_by(map[char]) 
+      end
+      cell
+    }
+    board
   end
   
+end
+
+def string_to_board(str)
+  MyBoards.to_board(str)
 end
 
 def computer_move(board)
@@ -32,7 +36,7 @@ def computer_move(board)
   move.pick_cell
 end
 
-class ComputerMoveBase < MiniTest::Unit::TestCase
+class ComputerMoveCore < MiniTest::Unit::TestCase
 
   def test_can_only_occupy_available_spot
     board = MyBoards.full_board_but_one_cell(4)
@@ -49,12 +53,12 @@ class ComputerMoveBase < MiniTest::Unit::TestCase
   end	
 
   def test_make_computer_win_first
-	board = MyBoards.to_board("CC2HH5678")
+	board = string_to_board("CC2HH5678")
 	assert_equal computer_move(board), 2
   end	
 
   def test_random_move_when_4_is_occupied
-	board = MyBoards.to_board("0123H5678")
+	board = string_to_board("0123H5678")
 	assert_equal computer_move(board) != 4, true
   end	
 	
@@ -63,12 +67,12 @@ end
 class BestMoveToWin < MiniTest::Unit::TestCase
 	
   def test_for_two_in_a_row_get_third
-	board = MyBoards.to_board("CC23H5678")
+	board = string_to_board("CC23H5678")
 	assert_equal computer_move(board), 2
   end
 	
   def test_for_two_in_a_row_with_a_gap
-	board = MyBoards.to_board("C123H5C78")
+	board = string_to_board("C123H5C78")
 	assert_equal computer_move(board), 3
   end
 	
@@ -78,12 +82,12 @@ end
 class BlockHumansBestMoveToWin < MiniTest::Unit::TestCase
 	
   def test_block_human_from_winning_with_simple_complete_row
-	board = MyBoards.to_board("HH23C5678")
+	board = string_to_board("HH23C5678")
 	assert_equal computer_move(board), 2
   end
 	
   def test_block_human_from_winning_with_diagonal
-	board = MyBoards.to_board("H12CH5678")
+	board = string_to_board("H12CH5678")
 	assert_equal computer_move(board), 8
   end
 		
