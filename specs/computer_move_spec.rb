29eedd80de29110c6ab780bb777c477026249ea0
Wolfram Kriesing <wk@uxebu.com>
2015-09-09 22:46:16 +0200

    Fix indentation that I broke, somehow.

--- specs/computer_move_spec.rb
+++ specs/computer_move_spec.rb
@@ -24,17 +24,17 @@ class ComputerMoveCore < MiniTest::Unit::TestCase
   end
 
   def test_place_on_cell_4_if_available
-	assert_equal computer_move(Boards.empty_board), 4
+  	assert_equal computer_move(Boards.empty_board), 4
   end	
 
   def test_make_computer_win_first
-	board = string_to_board("CC2HH5678")
-	assert_equal computer_move(board), 2
+    board = string_to_board("CC2HH5678")
+    assert_equal computer_move(board), 2
   end	
 
   def test_random_move_when_4_is_occupied
-	board = string_to_board("0123H5678")
-	assert_equal computer_move(board) != 4, true
+    board = string_to_board("0123H5678")
+    assert_equal computer_move(board) != 4, true
   end	
 	
 end
@@ -42,13 +42,13 @@ end
 class BestMoveToWin < MiniTest::Unit::TestCase
 	
   def test_for_two_in_a_row_get_third
-	board = string_to_board("CC23H5678")
-	assert_equal computer_move(board), 2
+    board = string_to_board("CC23H5678")
+    assert_equal computer_move(board), 2
   end
 	
   def test_for_two_in_a_row_with_a_gap
-	board = string_to_board("C123H5C78")
-	assert_equal computer_move(board), 3
+    board = string_to_board("C123H5C78")
+    assert_equal computer_move(board), 3
   end
 	
 end
@@ -57,13 +57,13 @@ end
 class BlockHumansBestMoveToWin < MiniTest::Unit::TestCase
 	
   def test_block_human_from_winning_with_simple_complete_row
-	board = string_to_board("HH23C5678")
-	assert_equal computer_move(board), 2
+    board = string_to_board("HH23C5678")
+    assert_equal computer_move(board), 2
   end
 	
   def test_block_human_from_winning_with_diagonal
-	board = string_to_board("H12CH5678")
-	assert_equal computer_move(board), 8
+    board = string_to_board("H12CH5678")
+    assert_equal computer_move(board), 8
   end
 		
 end
