89780f9d8a62dc367378fb3c0303c0e3d67f5405
Wolfram Kriesing <wk@uxebu.com>
2015-09-09 22:25:40 +0200

    Move production code out of test file.

--- specs/winner_spec.rb
+++ specs/winner_spec.rb
@@ -1,4 +1,5 @@
 require_relative "./boards"
+require_relative "../lib/winner"
 require "minitest/autorun"
 
 def find_winner_for(board)
@@ -37,34 +38,4 @@ class NoWinner < MiniTest::Unit::TestCase
     assert_equal winner, Boards.player1
   end
 
-end
-
-require_relative "../lib/game_rules"
-class Winner
-	
-  def initialize(board)
-    @board = board
-  end
-	
-  def find
-    player_for_win_combo(6, 7, 8) or
-    player_for_win_combo(3, 4, 5) or
-    player_for_win_combo(0, 1, 2) or
-    player_for_win_combo(0, 4, 8) or
-    player_for_win_combo(2, 4, 6) or
-    nil
-  end
-  
-  def player_for_win_combo(cell1, cell2, cell3)
-    if cells_equal?(cell1, cell2, cell3)
-      return @board[cell1].player
-    end
-  end
-  
-  def cells_equal?(cell1, cell2, cell3)
-    @board[cell1].owned_by_same_player?(@board[cell2]) && 
-    @board[cell2].owned_by_same_player?(@board[cell3])
-  end
-
-	
 end
