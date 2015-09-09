88d188b2fb74919fdae90c332538788c483add29
Wolfram Kriesing <wk@uxebu.com>
2015-09-09 22:46:26 +0200

    Extract diagonal win.

--- lib/winner.rb
+++ lib/winner.rb
@@ -8,8 +8,7 @@ class Winner
   def find
     player_for_row_win or
     player_for_column_win or
-    player_for_win_combo(0, 4, 8) or
-    player_for_win_combo(2, 4, 6) or
+    player_for_diagonal_win or
     nil
   end
   
@@ -23,6 +22,11 @@ class Winner
     player_for_win_combo(0, 3, 6) or
     player_for_win_combo(1, 4, 7) or
     player_for_win_combo(2, 5, 8)
+  end
+  
+  def player_for_diagonal_win
+    player_for_win_combo(0, 4, 8) or
+    player_for_win_combo(2, 4, 6)
   end
   
   def player_for_win_combo(cell1, cell2, cell3)
