0784c1a74f73e166d44afca9c6cd641f756d072c
Wolfram Kriesing <wk@uxebu.com>
2015-09-09 11:46:25 +0200

    Implement Opponents class.

--- specs/all_tests.rb
+++ specs/all_tests.rb
@@ -1,5 +1,5 @@
 require_relative "./computer_move_spec"
-require_relative "./user_move_spec"
+require_relative "./opponents_spec"
 require_relative "./game_rules_spec"
 require_relative "./winning_move_spec"
 require_relative "./random_move_spec"
--- specs/opponents_spec.rb
+++ specs/opponents_spec.rb
@@ -10,6 +10,11 @@ def default_game_players
   opponents.players
 end
 
+def players_for_game_type(game_type)
+  opponents = Opponents.new(PLAYER_1, PLAYER_2, game_type)
+  opponents.players
+end
+
 class OpponentsCharacters < MiniTest::Unit::TestCase
 
   def test_passes_the_players_character_to_first_player
@@ -38,14 +43,85 @@ class GameTypeDefault < MiniTest::Unit::TestCase
 	
 end
 
+class HumanVsComputer < MiniTest::Unit::TestCase
+
+  def test_first_player_is_human
+    player = players_for_game_type(Opponents::HUMAN_VS_COMPUTER).first
+    assert_equal player.instance_of?(HumanPlayer), true
+  end
+	
+  def test_second_player_is_computer
+    player = players_for_game_type(Opponents::HUMAN_VS_COMPUTER)[1]
+    assert_equal player.instance_of?(ComputerPlayer), true
+  end
+	
+end
+
+class ComputerVsHuman < MiniTest::Unit::TestCase
+
+  def test_first_player_is_human
+    player = players_for_game_type(Opponents::COMPUTER_VS_HUMAN).first
+    assert_equal player.instance_of?(ComputerPlayer), true
+  end
+	
+  def test_second_player_is_computer
+    player = players_for_game_type(Opponents::COMPUTER_VS_HUMAN)[1]
+    assert_equal player.instance_of?(HumanPlayer), true
+  end
+	
+end
+
+class ComputerVsComputer < MiniTest::Unit::TestCase
+
+  def test_first_player_is_human
+    player = players_for_game_type(Opponents::COMPUTER_VS_COMPUTER).first
+    assert_equal player.instance_of?(ComputerPlayer), true
+  end
+	
+  def test_second_player_is_computer
+    player = players_for_game_type(Opponents::COMPUTER_VS_COMPUTER)[1]
+    assert_equal player.instance_of?(ComputerPlayer), true
+  end
+	
+end
+
+class HumanVsHuman < MiniTest::Unit::TestCase
+
+  def test_first_player_is_human
+    player = players_for_game_type(Opponents::HUMAN_VS_HUMAN).first
+    assert_equal player.instance_of?(HumanPlayer), true
+  end
+	
+  def test_second_player_is_computer
+    player = players_for_game_type(Opponents::HUMAN_VS_HUMAN)[1]
+    assert_equal player.instance_of?(HumanPlayer), true
+  end
+	
+end
+
 class Opponents
 
-  def initialize(player1_char, player2_char)
+  HUMAN_VS_COMPUTER = 0
+  COMPUTER_VS_HUMAN = 1
+  HUMAN_VS_HUMAN = 2
+  COMPUTER_VS_COMPUTER = 3
+  
+  def initialize(player1_char, player2_char, game_type=HUMAN_VS_COMPUTER)
     @player1_char = player1_char
     @player2_char = player2_char
+    @game_type = game_type
   end
 	
   def players
+    if @game_type == COMPUTER_VS_HUMAN
+      return [ComputerPlayer.new(@player2_char), HumanPlayer.new(@player1_char)]
+    end 
+    if @game_type == COMPUTER_VS_COMPUTER
+      return [ComputerPlayer.new(@player2_char), ComputerPlayer.new(@player1_char)]
+    end 
+    if @game_type == HUMAN_VS_HUMAN
+      return [HumanPlayer.new(@player2_char), HumanPlayer.new(@player1_char)]
+    end 
 	  [HumanPlayer.new(@player1_char), ComputerPlayer.new(@player2_char)]
   end
   
