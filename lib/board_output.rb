770aaab5390cf8378111832bcde462a2d0d122d5
Wolfram Kriesing <wk@uxebu.com>
2015-09-16 22:02:26 +0200

    Generic boarder.

--- lib/board_output.rb
+++ lib/board_output.rb
@@ -7,37 +7,44 @@ class BoardOutput
   end
   
   def print(board)
+    @board = board
     print_empty_line
     print_board_border
-    board.rows_count.times do |row_index|
-      print_row(board, row_index)
-    end
+    print_rows
     print_empty_line
   end
 
   private
   
-  def print_row(board, row_index)
-    offset = row_index * board.rows_count
-    print_board_line(board, [offset + 0, offset + 1, offset + 2])
+  def print_rows
+    @board.rows_count.times do |row_index|
+      print_row(row_index)
+    end
+  end
+  
+  def print_row(row_index)
+    offset = row_index * @board.rows_count
+    print_board_line([offset + 0, offset + 1, offset + 2])
     print_board_border
   end
   
-  def print_board_line(board, cell_indexes)
+  def print_board_line(cell_indexes)
     line = "|"
     cell_indexes.each do |cell_index|
-      line += " #{print_cell(board, cell_index)} |"
+      line += " #{print_cell(cell_index)} |"
     end
     print_line line
   end
   
-  def print_cell(board, index)
-    cell = board.cells[index]
+  def print_cell(index)
+    cell = @board.cells[index]
     cell.is_available? ? index.to_s : cell.player
   end
   
   def print_board_border
-    print_line "+---+---+---+"
+    line = "+"
+    @board.rows_count.times { line += "---+" }
+    print_line line
   end
   
   def print_line(line)
