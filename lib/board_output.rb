aa5b0db56864d948f53b5f1be3e27dc02ce2797c
Wolfram Kriesing <wk@uxebu.com>
2015-09-16 21:55:44 +0200

    More generic board.

--- lib/board_output.rb
+++ lib/board_output.rb
@@ -9,16 +9,16 @@ class BoardOutput
   def print(board)
     print_empty_line
     print_board_border
-    print_row(board, 0)
-    print_row(board, 1)
-    print_row(board, 2)
+    board.rows_count.times do |row_index|
+      print_row(board, row_index)
+    end
     print_empty_line
   end
 
   private
   
   def print_row(board, row_index)
-    offset = row_index * 3
+    offset = row_index * board.rows_count
     print_board_line(board, [offset + 0, offset + 1, offset + 2])
     print_board_border
   end
