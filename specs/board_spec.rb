4e0dc9b0f482f704d7de7f66b26bc8cdd58e977f
Wolfram Kriesing <wk@uxebu.com>
2015-09-17 12:35:06 +0200

    Moved some game rules tests here, remove duplicates on the way.

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -25,3 +25,68 @@ class BoardTests < MiniTest::Unit::TestCase
   end
   
 end
+
+class GameNotWon < MiniTest::Unit::TestCase
+	
+  def test_no_cell_occupied_game_is_not_won
+	  assert_equal Boards.empty_board.won?, false  
+  end
+	
+  def test_tie_game_is_not_won
+	  assert_equal Boards.tie_board.won?, false  
+  end
+
+end
+
+class GameWon < MiniTest::Unit::TestCase
+	
+  def test_top_row_occupied_by_player_is_won
+	  assert_equal Boards.top_row_won_by_player1.won?, true  
+  end
+	
+  def test_bottom_row_occupied_by_player_is_won
+	  assert_equal Boards.bottom_row_won_by_player1.won?, true  
+  end
+
+  def test_diagonal_from_left_top_occupied_by_player_is_won
+	  assert_equal Boards.diagonal_from_left_top_won_by_player1.won?, true  
+  end
+
+  def test_diagonal_from_right_top_occupied_by_player_is_won
+	  assert_equal Boards.diagonal_from_right_top_won_by_player2.won?, true  
+  end
+
+end
+
+class Tie < MiniTest::Unit::TestCase
+
+  def test_no_cell_occupied_game_is_not_tie
+	  assert_equal Boards.empty_board.tie?, false  
+  end
+
+  def test_all_cells_occupied_is_tie
+	  assert_equal Boards.tie_board.tie?, true
+  end
+
+  def test_won_game_with_full_board_is_not_a_tie
+    board = Boards.tie_board
+    player1 = Boards.player1
+    board.make_move_to(0, player1)
+    board.make_move_to(1, player1)
+    board.make_move_to(2, player1)
+    assert_equal board.tie?, false
+  end
+
+end
+
+class GameOver < MiniTest::Unit::TestCase
+
+  def test_no_cell_occupied_game_is_not_over
+	 assert_equal Boards.empty_board.game_over?, false  
+  end
+
+  def test_tie_board_is_game_over
+  	assert_equal Boards.tie_board.game_over?, true
+  end
+
+end
