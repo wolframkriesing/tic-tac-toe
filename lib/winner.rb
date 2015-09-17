12971fb05d8303fe6cc956f79b5a7b9d28a8a766
Wolfram Kriesing <wk@uxebu.com>
2015-09-17 12:02:59 +0200

    Refactoring.

--- lib/winner.rb
+++ lib/winner.rb
@@ -15,11 +15,29 @@ class Winner
   private
   
   def player_for_row_win
+    winning_rows.first
+  end
+  
+  def player_for_column_win
+    winning_columns.first
+  end
+
+  def player_for_diagonal_win
+    return player_for_diagonal_left_top_to_right_bottom || player_for_diagonal_right_top_to_left_bottom
+  end
+    
+  def winning_rows
     rows_count = @board.rows_count
     (0..rows_count-1)
       .map {|row_index| player_for_winning_row(row_index) }
       .select {|player| player != nil}
-      .first
+  end
+  
+  def winning_columns
+    columns_count = @board.columns_count
+    (0..columns_count-1)
+      .map {|column_index| player_for_winning_column(column_index) }
+      .select {|player| player != nil}
   end
   
   def player_for_winning_row(row_index)
@@ -29,34 +47,31 @@ class Winner
     player_for_win_combo(cell_indexes)
   end
   
-  def player_for_column_win
-    columns_count = @board.columns_count
-    (0..columns_count-1)
-      .map {|column_index| player_for_winning_column(column_index) }
-      .select {|player| player != nil}
-      .first
-  end
-  
   def player_for_winning_column(column_index)
     columns_count = @board.columns_count
     cell_indexes = (0..columns_count-1).map {|index| column_index + index*columns_count}
     player_for_win_combo(cell_indexes)
   end
   
-  def player_for_diagonal_win
-    return player_for_diagonal_left_top_to_right_bottom || player_for_diagonal_right_top_to_left_bottom
-  end
-  
   def player_for_diagonal_left_top_to_right_bottom
     rows_count = @board.rows_count
-    cell_indexes = (0..rows_count-1).map {|index| index + index*rows_count}
+    cell_indexes = (0..rows_count-1).map {|i| cell_indexes_diagonal_from_left_top(i) }
     player_for_win_combo(cell_indexes)
   end
   
   def player_for_diagonal_right_top_to_left_bottom
     rows_count = @board.rows_count
-    cell_indexes = (1..rows_count).map {|index| index*rows_count - index}
+    cell_indexes = (0..rows_count-1).map {|i| cell_indexes_diagonal_from_right_top(i) }
     player_for_win_combo(cell_indexes)
+  end
+  
+  def cell_indexes_diagonal_from_left_top(index)
+    index * @board.rows_count + index
+  end
+  
+  def cell_indexes_diagonal_from_right_top(index)
+    next_index = index + 1
+    next_index * @board.rows_count - next_index
   end
   
   def player_for_win_combo(cell_indexes)    
