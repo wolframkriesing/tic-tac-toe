de3fb447292d054765e357862d998a6563fd53b1
Wolfram Kriesing <wk@uxebu.com>
2015-09-17 10:50:09 +0200

    No magic numbers.

--- lib/colorize.rb
+++ lib/colorize.rb
@@ -1,15 +1,18 @@
 class Colorize
 
+  RED = 31
+  GREEN = 32
+
   def self.colorize(character, color_code)
     "\e[#{color_code}m#{character}\e[0m"
   end
 
   def self.red(character)
-    colorize(character, 31)
+    colorize(character, RED)
   end
 
   def self.green(character)
-    colorize(character, 32)
+    colorize(character, GREEN)
   end
   
 end
