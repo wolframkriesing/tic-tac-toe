2d2b997cd8feb781e4febb426e0365ade43cbb31
Wolfram Kriesing <wk@uxebu.com>
2015-09-17 11:02:20 +0200

    Use more generic function.

--- lib/winner.rb
+++ lib/winner.rb
@@ -24,7 +24,7 @@ class Winner
   
   def player_for_winning_row(row_index, rows_count)
     offset = row_index * rows_count
-    player_for_win_combo(offset + 0, offset + 1, offset + 2)
+    player_for_win_combo([offset + 0, offset + 1, offset + 2])
   end
   
   def player_for_column_win
@@ -36,21 +36,15 @@ class Winner
   end
   
   def player_for_winning_column(column_index)
-    player_for_win_combo(column_index + 0, column_index + 3, column_index + 6)
+    player_for_win_combo([column_index + 0, column_index + 3, column_index + 6])
   end
   
   def player_for_diagonal_win
-    player_for_win_combo(0, 4, 8) or
-    player_for_win_combo(2, 4, 6)
+    player_for_win_combo([0, 4, 8]) or
+    player_for_win_combo([2, 4, 6])
   end
   
-  def player_for_win_combo(cell1, cell2, cell3)    
-    # first_cell = @board.cells[cell1]
-    # first_cell.player if cells_owned_by_same_player?([first_cell, @board.cells[cell2], @board.cells[cell3]])
-    _player_for_win_combo([cell1, cell2, cell3])
-  end
-  
-  def _player_for_win_combo(cell_indexes)    
+  def player_for_win_combo(cell_indexes)    
     first_cell = @board.cells[cell_indexes[0]]
     cells = cell_indexes.map {|cell_index| @board.cells[cell_index] }
     first_cell.player if cells_owned_by_same_player?(cells)
