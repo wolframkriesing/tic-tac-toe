1861200593aaa48d195895b6a84de9c9ce1dbaff
Wolfram Kriesing <wk@uxebu.com>
2015-09-17 18:04:03 +0200

    Render the immutable board properly.

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,7 @@ class Game
   
   def start_game(board)
     @game_screens = GameScreens.new
-    game_play(board)
+    board = game_play(board)
     @game_screens.game_over(board)
   end
   
@@ -20,19 +20,18 @@ class Game
     @game_screens.game_start(board)
     game_play = GamePlay.new(@player1, @player2)
     while !board.game_over?
-      until valid_move(board, game_play)
-      end
+      until new_board = board_after_valid_move(board, game_play); end
+      board = new_board
       @game_screens.next_move_screen(board)
     end
+    board
   end
   
-  def valid_move(board, game_play)
+  def board_after_valid_move(board, game_play)
     begin
-      game_play.next_move(board)
-      return true
+      return game_play.next_move(board)
     rescue InvalidMoveError => err
-      @game_screens.invalid_cell(err.picked_cell_index) 
-      return false
+      @game_screens.invalid_cell(err.picked_cell_index)
     end
   end
   
